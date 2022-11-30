d8e43eb1a9e891e7c001ff42bd50f3599a035d35
Beth Rissman <beth.rissman@telepham.com>
2022-11-29 21:00:27 -0600

    removed ../ on images

--- js/app.js
+++ js/app.js
@@ -92,25 +92,25 @@ function renderResults(){
   }
 }
 
-new Product('bag', '../images/bag.jpg');
-new Product('banana', '../images/banana.jpg');
-new Product('bathroom', '../images/bathroom.jpg');
-new Product('boots', '../images/boots.jpg');
-new Product('breakfast', '../images/breakfast.jpg');
-new Product('bubblegum', '../images/bubblegum.jpg');
-new Product('chair', '../images/chair.jpg');
-new Product('cthulhu', '../images/cthulhu.jpg');
-new Product('dog-duck', '../images/dog-duck.jpg');
-new Product('dragon', '../images/dragon.jpg');
-new Product('pen', '../images/pen.jpg');
-new Product('pet-sweep', '../images/pet-sweep.jpg');
-new Product('scissors', '../images/scissors.jpg');
-new Product('shark', '../images/shark.jpg');
-new Product('sweep', '../images/sweep.png');
-new Product('tauntaun', '../images/tauntaun.jpg');
-new Product('unicorn', '../images/unicorn.jpg');
-new Product('water-can', '../images/water-can.jpg');
-new Product('wine-glass', '../images/wine-glass.jpg');
+new Product('bag', 'images/bag.jpg');
+new Product('banana', 'images/banana.jpg');
+new Product('bathroom', 'images/bathroom.jpg');
+new Product('boots', 'images/boots.jpg');
+new Product('breakfast', 'images/breakfast.jpg');
+new Product('bubblegum', 'images/bubblegum.jpg');
+new Product('chair', 'images/chair.jpg');
+new Product('cthulhu', 'images/cthulhu.jpg');
+new Product('dog-duck', 'images/dog-duck.jpg');
+new Product('dragon', 'images/dragon.jpg');
+new Product('pen', 'images/pen.jpg');
+new Product('pet-sweep', 'images/pet-sweep.jpg');
+new Product('scissors', 'images/scissors.jpg');
+new Product('shark', 'images/shark.jpg');
+new Product('sweep', 'images/sweep.png');
+new Product('tauntaun', 'images/tauntaun.jpg');
+new Product('unicorn', 'images/unicorn.jpg');
+new Product('water-can', 'images/water-can.jpg');
+new Product('wine-glass', 'images/wine-glass.jpg');
 
 
 renderProducts();
